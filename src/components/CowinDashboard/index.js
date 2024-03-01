6eb99f84118691f810f91b363b87cebb927b40b7
budaykumar <128334885+budaykumar@users.noreply.github.com>
2024-03-01 16:26:16 +0530

    Update index.js

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -34,7 +34,7 @@ class CowinDashboard extends Component {
           eachDayData => ({
             vaccineDate: eachDayData.vaccine_date,
             dose1: eachDayData.dose_1,
-            dose2: eachDayData.dose2,
+            dose2: eachDayData.dose_2,
           }),
         ),
         vaccinationByAge: fetchedData.vaccination_by_age.map(range => ({
